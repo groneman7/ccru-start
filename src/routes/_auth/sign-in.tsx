8e9dd8665606771065ca72d02e180665a39e2eee
Evan Groneman <groneman7@gmail.com>
2026-02-17 20:46:17 -0500

    clean up

--- src/routes/_auth/sign-in.tsx
+++ src/routes/_auth/sign-in.tsx
@@ -1,6 +1,12 @@
 // import { usePostHog } from '@posthog/react';
 import { IconCheck, IconSparkles } from '@tabler/icons-react';
-import { createFileRoute, Link, redirect } from '@tanstack/react-router';
+import {
+  createFileRoute,
+  Link,
+  redirect,
+  useNavigate,
+  useRouter,
+} from '@tanstack/react-router';
 import { createServerFn } from '@tanstack/react-start';
 import { useAppForm } from '~/components/form';
 import {
@@ -42,7 +48,9 @@ export const Route = createFileRoute('/_auth/sign-in')({
 });
 
 function RouteComponent() {
-  // const posthog = usePostHog();
+  const nav = useNavigate();
+  const router = useRouter();
+
   const form = useAppForm({
     defaultValues: {
       email: '',
@@ -51,13 +59,29 @@ function RouteComponent() {
     },
     onSubmit: async ({ value }) => {
       const { email, password, staySignedIn } = value;
-      const test = await authClient.signIn.email({
-        email,
-        password,
-        rememberMe: staySignedIn,
-      });
 
-      console.log(test);
+      await authClient.signIn.email(
+        {
+          email,
+          password,
+          rememberMe: staySignedIn,
+        },
+        {
+          onRequest: () => {
+            setOtpLoading(true);
+          },
+          onSuccess: async () => {
+            setOtpLoading(false);
+            await router.invalidate();
+            await nav({ to: '/' });
+          },
+          onError: (ctx) => {
+            setOtpLoading(false);
+            console.log(ctx.error);
+            alert(ctx.error.message);
+          },
+        },
+      );
     },
   });
 
@@ -75,18 +99,10 @@ function RouteComponent() {
         },
         onSuccess: () => {
           setOtpLoading(false);
-          // posthog.capture('user_signed_in', {
-          //   provider: 'google',
-          // });
         },
         onError: (ctx) => {
           setOtpLoading(false);
           console.log(ctx.error);
-          // posthog.capture('sign_in_failed', {
-          //   provider: 'google',
-          //   error_message: ctx.error.message,
-          // });
-          // posthog.captureException(ctx.error);
           alert(ctx.error.message);
         },
       },
@@ -115,7 +131,6 @@ function RouteComponent() {
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            handleSignIn();
           }}
           className="grid gap-4"
         >
@@ -198,38 +213,6 @@ function RouteComponent() {
             </svg>
             Sign in with Google
           </Button>
-          {/* <Button
-            className="flex-1"
-            disabled={otpLoading}
-            type="button"
-            variant="outline"
-            onClick={handleGoogleSignIn}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="0.98em"
-              height="1em"
-              viewBox="0 0 256 262"
-            >
-              <path
-                fill="#4285F4"
-                d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
-              />
-              <path
-                fill="#34A853"
-                d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055c-34.523 0-63.824-22.773-74.269-54.25l-1.531.13l-40.298 31.187l-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
-              />
-              <path
-                fill="#FBBC05"
-                d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82c0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602z"
-              />
-              <path
-                fill="#EB4335"
-                d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0C79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
-              />
-            </svg>
-            Google
-          </Button> */}
         </div>
       </CardContent>
     </Card>
