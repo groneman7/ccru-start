e962cf543a023b489870fbda8bf74b51efff6cc2
Evan Groneman <groneman7@gmail.com>
2026-02-17 20:41:20 -0500

    add checkbox component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,6 +2,7 @@ export * from './badge';
 export * from './button';
 export * from './button-group';
 export * from './card';
+export * from './checkbox';
 export * from './combobox';
 export * from './command';
 export * from './date-picker';
