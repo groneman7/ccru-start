e55d67db77e099b7a66b5c407fe91013658f7c4d
Evan Groneman <groneman7@gmail.com>
2026-02-18 21:47:18 -0500

    fix potential hydration problem

--- src/components/coming-soon-tooltip.tsx
+++ src/components/coming-soon-tooltip.tsx
@@ -1,12 +1,13 @@
 import { Tooltip, TooltipContent, TooltipTrigger } from '~/components/ui';
-import type { ReactNode } from 'react';
+import type { ReactElement } from 'react';
 
-export function ComingSoonTooltip({ children }: { children: ReactNode }) {
+export function ComingSoonTooltip({ children }: { children: ReactElement }) {
   return (
     <Tooltip>
-      <TooltipTrigger className="cursor-not-allowed! **:cursor-not-allowed!">
-        {children}
-      </TooltipTrigger>
+      <TooltipTrigger
+        className="cursor-not-allowed! **:cursor-not-allowed!"
+        render={children}
+      />
       <TooltipContent sideOffset={8}>
         This feature isn't available yet, but I am actively working on it.
         Please check back soon!
